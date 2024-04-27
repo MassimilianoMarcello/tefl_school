b3d0a28d987a65203523f6ec44f1c8235d34eaf6
massimiliano <massimarcello@hotmail.it>
2024-04-27 22:17:46 +0200

    DESIGN

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import s from "./page.module.scss";
 import SlideItem from "@/components/SlideItem/SlideItem";
 import type { SlideItem as SlideItemType } from "@/Types/SlideItem"; 
+// import B_Design from "@/components/MainPage/B_Design";
 
 
 export default function Home() {
   return (
     <main className={s.main}>
-       <SlideItem />
+       <SlideItem  />
+       {/* <B_Design/> */}
      <h1 className={s.h1}>tefl website</h1>
    
     </main>
