2b0c3058e68761b0b02408ebdddb61ea411287e1
massimiliano <massimarcello@hotmail.it>
2024-04-29 02:01:13 +0200

    parallel fetching

--- components/MainPage/_MainPageWrapper.tsx
+++ components/MainPage/_MainPageWrapper.tsx
@@ -1,52 +1,117 @@
-// import FirstComponent from './A_FirstComponent.tsx';
-// // import SecondComponent from './B_SecondComponent.jsx';
-// import SecondComponent from "./B_SecondComponent";
-
-// const _MainPageWrapper = () => {
-//   return (<>
-//   <FirstComponent/>
-//   <SecondComponent/>
-//   </>
-   
-//   )
-// }
-
-// export default _MainPageWrapper
 
 
-// import { useEffect, useState } from 'react';
+
+
+
 // import { getHomePage } from '@/sanity/sanity.query';
 // import FirstComponent from './A_FirstComponent.tsx';
 // import SecondComponent from "./B_SecondComponent";
-// import React from 'react';
 
 
+
+// const MainPageWrapper = async () => {
+//   try {
+//     const [firstComponentData, secondComponentData] = await Promise.all([
+//       getHomePage(),
+//       getHomePage()
+//     ]);
+
+//     // Verifica che entrambi i set di dati non siano nulli
+//     if (!firstComponentData || !secondComponentData) {
+//       console.error('Errore nel recupero dei dati: uno o entrambi i set di dati sono nulli');
+//       return null;
+//     }
+
+//     return (
+//       <>
+//         {/* Passa i dati come props ai componenti figlio */}
+//         <FirstComponent data={firstComponentData} />
+//         <SecondComponent data={secondComponentData} />
+//       </>
+//     );
+//   } catch (error) {
+//     console.error('Errore nel recupero dei dati:', error);
+//     return null;
+//   }
+// };
+
+// export default MainPageWrapper;
+
+
+
+// import { getHomePage } from '@/sanity/sanity.query';
+// import FirstComponent from './A_FirstComponent.tsx';
+// import SecondComponent from "./B_SecondComponent";
+
+// const getHomePageData = async () => {
+//   try {
+//     return await getHomePage();
+//   } catch (error) {
+//     console.error('Errore nel recupero dei dati di HomePage:', error);
+//     return null;
+//   }
+// };
+
+// const MainPageWrapper = async () => {
+//   try {
+//     const components = [FirstComponent, SecondComponent]; // Array dei componenti da includere
+  
+//     // Genera un array di promesse, una per ogni componente, chiamando getHomePageData()
+//     const homePageDataPromises = components.map(Component => getHomePageData());
+  
+//     const componentDataArray = await Promise.all(homePageDataPromises);
+  
+//     // Verifica che tutti i set di dati non siano nulli
+//     if (componentDataArray.some((data: any) => !data)) {
+//       console.error('Errore nel recupero dei dati: uno o più set di dati sono nulli');
+//       return null;
+//     }
+  
+//     return (
+//       <>
+//         {/* Mappa i dati dei componenti e passali come props ai componenti figlio */}
+//         {components.map((Component, index) => (
+//           <Component key={index} data={componentDataArray[index]} />
+//         ))}
+//       </>
+//     );
+//   } catch (error) {
+//     console.error('Errore nel recupero dei dati:', error);
+//     return null;
+//   }
+// };
+
+// export default MainPageWrapper;
 
 
 import { getHomePage } from '@/sanity/sanity.query';
 import FirstComponent from './A_FirstComponent.tsx';
 import SecondComponent from "./B_SecondComponent";
+import { HomePage } from '@/Types/Homepage';
 
 
 
 const MainPageWrapper = async () => {
   try {
-    const [firstComponentData, secondComponentData] = await Promise.all([
-      getHomePage(),
-      getHomePage()
-    ]);
-
-    // Verifica che entrambi i set di dati non siano nulli
-    if (!firstComponentData || !secondComponentData) {
-      console.error('Errore nel recupero dei dati: uno o entrambi i set di dati sono nulli');
+    const components = [FirstComponent, SecondComponent]; // Array dei componenti da includere
+  
+    // Genera un array di promesse, una per ogni componente, chiamando getHomePage()
+    const homePageDataPromises = components.map(Component => getHomePage());
+  
+    const componentDataArray = await Promise.all(homePageDataPromises);
+  
+    // Verifica che tutti i set di dati non siano nulli
+    if (componentDataArray.some((data: any) => !data)) {
+      console.error('Errore nel recupero dei dati: uno o più set di dati sono nulli');
       return null;
     }
-
+  
     return (
       <>
-        {/* Passa i dati come props ai componenti figlio */}
-        <FirstComponent data={firstComponentData} />
-        <SecondComponent data={secondComponentData} />
+        {/* Mappa i dati dei componenti e passali come props ai componenti figlio */}
+        {components.map((Component, index) => (
+          <Component key={index} data={componentDataArray[index]} />
+        ))}
       </>
     );
   } catch (error) {
