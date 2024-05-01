07b5886c6b4d1262a268a1a3606db3827c590932
massimiliano <massimarcello@hotmail.it>
2024-05-01 23:25:24 +0200

    styling courses cards

--- components/MainPage/D_CoursesPreview.tsx
+++ components/MainPage/D_CoursesPreview.tsx
@@ -1,39 +1,50 @@
-import React from 'react'
-import { Course } from '@/Types/Course';
+import { Course } from "@/Types/Course";
 import Image from "next/image";
+import styles from "./D_CoursesPreview.module.scss";
+import Link from 'next/dist/client/link'
 
 
 
-  
-  interface CourseProps {
-    data: Course[]; // Accetta i dati come prop
-  }
-  
-  export default function D_CoursesPreview({ data }: CourseProps) {
-    // Ordina i dati in base all'indice inverso per ottenere gli ultimi progetti
-    const sortedData = data.sort((a, b) => data.indexOf(b) - data.indexOf(a));
-    // Prendi solo gli ultimi 4 progetti
-    const latestProjects = sortedData.slice(0, 4);
-  
-    return (
-      <main className="pannarru">
-        <section className="busicco">
-          {/* Itera solo sugli ultimi 4 progetti */}
-          {latestProjects.map((course) => (
-            <div key={course._id} className="gavini">
-              <h1 className="alicion">{course.name}</h1>
-              <p className="tamarindo">{course.description}</p>
-              <Image src={course.photo} alt={course.name} width={300} height={300} />
-              <h1 className="alicion">{course.completionTimeWeeks}</h1>
-              <h1 className="alicion">{course.hours}</h1>
-              <h1 className="alicion">{course.startDate}</h1>
-              <h1 className="alicion">{course.endDate}</h1>
-              <h1 className="alicion">{course.price}</h1>
-              <h1 className="alicion">{course.level}</h1>
+interface CourseProps {
+  data: Course[]; // Accetta i dati come prop
+}
+
+export default function D_CoursesPreview({ data }: CourseProps) {
+  // Ordina i dati in base all'indice inverso per ottenere gli ultimi progetti
+  const sortedData = data.sort((a, b) => data.indexOf(b) - data.indexOf(a));
+  // Prendi solo gli ultimi 4 progetti
+  const latestProjects = sortedData.slice(0, 4);
+
+  return (
+    <main className={styles.mainContainer}>
+      <section className={styles.coursesSection}>
+        {/* Itera solo sugli ultimi 4 progetti */}
+        {latestProjects.map((course) => (
+          <div key={course._id} className={styles.cardContainer}>
+            <div className={styles.cardImageContainer}>
+              <Image
+                className={styles.cardImage}
+                src={course.photo}
+                alt={course.name}
+                width={500}
+                height={300}
+              />
             </div>
-          ))}
-        </section>
-      </main>
-    );
-  }
-  
+            <div className={styles.cardTextContainer}>
+              <h1 className={styles.cardTitle}>{course.name}</h1>
+              <p className={styles.cardDescription}>{course.description}</p>
+              <div className={styles.courseDetails}>
+                <p className={styles.courseDetail}>{course.startDate} - {course.endDate}</p>
+                <p className={styles.courseDetail}>{course.completionTimeWeeks} weeks</p>
+                <p className={styles.courseDetail}>{course.hours} hours</p>
+                <p className={styles.courseDetail}>Price: {course.price}</p>
+                <p className={styles.courseDetail}>Level: {course.level}</p>
+              </div>
+              <Link href={`/our_courses/${course.slug}`} className={styles.detailsLink}>Details</Link>
+            </div>
+          </div>
+        ))}
+      </section>
+    </main>
+  );
+}
