5fa114701573f1327a40ebd7129f779e2c6af24e
massimiliano <massimarcello@hotmail.it>
2024-05-05 18:30:13 +0200

    fixed squares

--- components/MainPage/C_ThirdComponent.tsx
+++ components/MainPage/C_ThirdComponent.tsx
@@ -16,11 +16,12 @@ export default function ThirdComponent({ data }: ThirdComponentProps) {
     
           <section className={styles.one_Main_Container}>
       <div className={styles.two_Section_Container}>
-      <span className={styles.squareOne}>1</span>
-              <span className={styles.squareTwo}>1</span>
+    
         {/* Renderizza solo il secondo componente della prima HomePage se data è definito */}
         {thirdComponent && (
           <div key={thirdComponent._key} className={styles.three_Map_Container}>
+              <span className={styles.squareOne}>1</span>
+              <span className={styles.squareTwo}>1</span>
             <div className={styles.textContainer}>
               <h1 className={styles.mainTitle}>{thirdComponent.title}</h1>
               <p className={styles.mainText}>{thirdComponent.text}</p>
