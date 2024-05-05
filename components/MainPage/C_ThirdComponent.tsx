3602aef439eaf6778d6bf9be2f26ff21080b9d4a
massimiliano <massimarcello@hotmail.it>
2024-05-05 04:05:38 +0200

    second compoent almost done

--- components/MainPage/C_ThirdComponent.tsx
+++ components/MainPage/C_ThirdComponent.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import type { HomePage } from '@/Types/Homepage';
-
+import styles from './C_ThirdComponent.module.scss';
 interface ThirdComponentProps {
     data: HomePage[]; // Accetta i dati come prop
   }
@@ -10,14 +10,15 @@ interface ThirdComponentProps {
     const thirdComponent = data && data[0]?.components[1];
   
     return (
-      <main className="pannarru">
-        <section className="busicco">
-          {/* Renderizza solo il secondo componente della prima HomePage se data è definito */}
-          {thirdComponent && (
-            <div key={thirdComponent._key} className="gavini">
-                <h1>kiss my</h1>
-              <h1 className="alicion">{thirdComponent.title}</h1>
-              <p className="tamarindo">{thirdComponent.text}</p>
+      <main className={styles.oneMainContainer}>
+           <section className={styles.twoSectionContainer}>
+           <span className={styles.square1}>1</span>
+              <span className={styles.square2}>1</span>
+                   {thirdComponent && (
+            <div key={thirdComponent._key} className={styles.threeMapContainer}>
+              
+              <h1 className={styles.mainTitle}>{thirdComponent.title}</h1>
+              <p className={styles.mainText}>{thirdComponent.text}</p>
               <Image src={thirdComponent.image} alt="Component Image" width={400} height={300} />
             </div>
           )}
