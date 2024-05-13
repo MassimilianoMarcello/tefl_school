5c6de1d6341839894689f1096895c41ec19a601a
massimiliano <massimarcello@hotmail.it>
2024-05-13 21:36:31 +0200

    essential style teachers done

--- components/Our teachers/OurTeachers.tsx
+++ components/Our teachers/OurTeachers.tsx
@@ -1,5 +1,6 @@
 import styles from "./teachers.module.scss";
 import { getTeachers } from "@/sanity/sanity.query";
+import { urlFor } from "@/sanity/sanity.client";
 
 import Image from "next/image";
 import { Teachers as TeachersType } from "@/Types/Teachers";
@@ -13,40 +14,49 @@ interface TeachersTypeProps {
 export default function OurTeachers({ data }: TeachersTypeProps) {
   revalidateTag("collection");
 
-
   return (
     <main className={styles.mainContainer}>
       <section className={styles.coursesTextContainer}>
- 
         <span className={styles.square3}></span>
         <span className={styles.square4}></span>
 
         <div className={styles.courseCardsBox}>
           {data &&
-           data.map((teacher, index) => (
-              <div key={teacher._id} className={`${styles.cardContainer} ${styles[`cardContainer${index}`]}`}>
+            data.map((teacher, index) => (
+              <div
+                key={teacher._id}
+                className={`${styles.cardContainer} ${styles[`cardContainer${index}`]}`}
+              >
                 <div className={styles.cardImageContainer}>
-                  <Image
+                  {/* <Image
                     className={styles.cardImage}
                     src={teacher.idPhoto}
                     alt={teacher.alt}
                     width={500}
                     height={300}
+                  /> */}
+                  <Image
+                    src={urlFor(teacher.idPhoto).width(300).height(300).url()}
+                    width={200}
+                    height={200}
+                    alt={teacher.alt}
+                    className={styles.cardImage}
                   />
 
                   <div className={styles.courseLevelContainer}>
-                    <p className={styles.courseLevel}>
+                    {/* <p className={styles.courseLevel}>
                       {teacher.infoParagraph}
-                    </p>
+                    </p> */}
                   </div>
                   <div className={styles.cardNameContainer}>
-                    <p className={styles.cardName}>{teacher.role}</p>
+                    <p className={styles.cardName}>{teacher.name}</p>
+                    <p className={styles.cardRole}>{teacher.role}</p>
                   </div>
                 </div>
 
-                <Link href={`/`} className={styles.detailsLink}>
+                {/* <Link href={`/`} className={styles.detailsLink}>
                   Details
-                </Link>
+                </Link> */}
               </div>
             ))}
         </div>
