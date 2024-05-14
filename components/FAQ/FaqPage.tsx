de69e20b7e97d35cadb5342318e472e8ba5972c3
massimiliano <massimarcello@hotmail.it>
2024-05-14 11:50:23 +0200

    toggle faq working

--- components/FAQ/FaqPage.tsx
+++ components/FAQ/FaqPage.tsx
@@ -1,55 +1,69 @@
-import React from 'react';
+'use client'
+
+
+import React, {useState} from 'react';
 import styles from './FaqPage.module.scss'; 
 import {Faq} from '@/Types/Faq'; 
 
 
 
+
+
+
+
+
 interface FaqPageProps {
-    faqs: Faq[];
-  }
-  
-  const FaqPage: React.FC<FaqPageProps> = ({ faqs }) => {
-    const generalFaqs = faqs.filter(faq => faq.category === 'general');
-    const technicalFaqs = faqs.filter(faq => faq.category === 'technical');
-    const livingCostsFaqs = faqs.filter(faq => faq.category === 'living costs');
-  
-    return (
-      <div className={styles.container}>
-        <div className={styles.box}>
-          <h2>General Questions</h2>
-          <ul>
-            {generalFaqs.map(faq => (
-              <li key={faq._id}>
-                <strong>{faq.question}</strong>
-                <p>{faq.answer}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className={styles.box}>
-          <h2>Paperwork and Documentation</h2>
-          <ul>
-            {technicalFaqs.map(faq => (
-              <li key={faq._id}>
-                <strong>{faq.question}</strong>
-                <p>{faq.answer}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className={styles.box}>
-          <h2>Questions about Florence: Living in Italy</h2>
-          <ul>
-            {livingCostsFaqs.map(faq => (
-              <li key={faq._id}>
-                <strong>{faq.question}</strong>
-                <p>{faq.answer}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    );
+  faqs: Faq[];
+}
+
+const FaqPage: React.FC<FaqPageProps> = ({ faqs }) => {
+  const [openFaqs, setOpenFaqs] = useState<{ [key: string]: boolean }>({});
+
+  const toggleFaq = (id: string) => {
+    setOpenFaqs(prevState => ({
+      ...prevState,
+      [id]: !prevState[id]
+    }));
   };
-  
-  export default FaqPage;
+
+  const generalFaqs = faqs.filter(faq => faq.category === 'general');
+  const technicalFaqs = faqs.filter(faq => faq.category === 'technical');
+  const livingCostsFaqs = faqs.filter(faq => faq.category === 'living costs');
+
+  const renderFaqs = (faqs: Faq[]) => (
+    <ul>
+      {faqs.map(faq => (
+        <li key={faq._id} className={`${styles.faqItem} ${openFaqs[faq._id] ? styles.open : ''}`}>
+          <div className={styles.question} onClick={() => toggleFaq(faq._id)}>
+            <strong>{faq.question}</strong>
+            <button className={styles.toggleButton}>
+              {openFaqs[faq._id] ? '-' : '+'}
+            </button>
+          </div>
+          <div className={styles.answerWrapper}>
+            <p className={styles.answer}>{faq.answer}</p>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.box}>
+        <h2>General Questions</h2>
+        {renderFaqs(generalFaqs)}
+      </div>
+      <div className={styles.box}>
+        <h2>Paperwork and Documentation</h2>
+        {renderFaqs(technicalFaqs)}
+      </div>
+      <div className={styles.box}>
+        <h2>Questions about Florence: Living in Italy</h2>
+        {renderFaqs(livingCostsFaqs)}
+      </div>
+    </div>
+  );
+};
+
+export default FaqPage;
