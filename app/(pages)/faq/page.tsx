a6fd68120a1a8eb656a6966f2cec240ab94dc3c9
massimiliano <massimarcello@hotmail.it>
2024-05-17 01:37:28 +0200

    faqpage top page added

--- app/(pages)/faq/page.tsx
+++ app/(pages)/faq/page.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import FaqPage from '@/components/FAQ/FaqPage';
 import { getAllFaqs } from '@/sanity/sanity.query';
 import { revalidateTag } from "next/cache";
+import FaqTopPageData from '@/components/FAQ/FaqTopPageData';
 
 
 
@@ -11,7 +12,12 @@ const Faq = async () => {
   const faqs = await getAllFaqs();
 
 
-  return <FaqPage faqs={faqs} />;
+  return <>
+  <FaqTopPageData/>
+    <FaqPage faqs={faqs} />;
+  </>
+  
+
 };
 
 export default Faq;
---
+++ components/FAQ/FaqTopPageData.tsx
@@ -0,0 +1,27 @@
+'use server'
+
+import { getPage } from "@/sanity/sanity.query";
+import WhyFlorence from '@/components/WhyFlorence/WhyFlorence';
+
+const FaqTopPageData = async () => {
+  try {
+    const pageData = await getPage(2); // Passa l'indice corretto
+
+    // Verifica che i dati non siano nulli
+    if (!pageData) {
+      console.error("Errore nel recupero dei dati: i dati della pagina sono nulli");
+      return null;
+    }
+
+    return (
+      <>
+        <WhyFlorence data={pageData} />
+      </>
+    );
+  } catch (error) {
+    console.error("Errore nel recupero dei dati:", error);
+    return null;
+  }
+};
+
+export default FaqTopPageData;
