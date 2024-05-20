43e214a644f78aa9a6f580cfa82888adc3085453
massimiliano <massimarcello@hotmail.it>
2024-05-20 04:22:45 +0200

    INDEX

--- components/Work_in_Italy/WorkInItalyData.tsx
+++ components/Work_in_Italy/WorkInItalyData.tsx
@@ -1,31 +1,31 @@
-"use server";
+// "use server";
 
 
 
-import { getPage } from "@/sanity/sanity.query";
-import Page from "@/components/Common_Page/Page";
+// import { getPage } from "@/sanity/sanity.query";
+// import Page from "@/components/Common_Page/Page";
 
-const WorkInItalyPageWrapper = async () => {
-  try {
-    const pageData = await getPage(1); // Passa l'indice corretto
+// const WorkInItalyPageWrapper = async () => {
+//   try {
+//     const pageData = await getPage(1); // Passa l'indice corretto
 
-    // Verifica che i dati non siano nulli
-    if (!pageData) {
-      console.error(
-        "Errore nel recupero dei dati: i dati della pagina sono nulli"
-      );
-      return null;
-    }
+//     // Verifica che i dati non siano nulli
+//     if (!pageData) {
+//       console.error(
+//         "Errore nel recupero dei dati: i dati della pagina sono nulli"
+//       );
+//       return null;
+//     }
 
-    return (
-      <>
-        <Page data={pageData} pageType={pageData.pageType.current} />
-      </>
-    );
-  } catch (error) {
-    console.error("Errore nel recupero dei dati:", error);
-    return null;
-  }
-};
+//     return (
+//       <>
+//         <Page data={pageData} pageType={pageData.pageType.current} pageI/>
+//       </>
+//     );
+//   } catch (error) {
+//     console.error("Errore nel recupero dei dati:", error);
+//     return null;
+//   }
+// };
 
-export default WorkInItalyPageWrapper;
+// export default WorkInItalyPageWrapper;
