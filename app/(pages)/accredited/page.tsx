fd7f849d38d0f2e4add04ce33a58ab9058e5ae33
massimiliano <massimarcello@hotmail.it>
2024-06-06 21:30:05 +0200

    accredited top page

--- app/(pages)/accredited/page.tsx
+++ app/(pages)/accredited/page.tsx
@@ -1,30 +1,94 @@
-"use server";
+// "use server";
 
-import { getAccredited } from "@/sanity/sanity.query";
-import Page from "@/components/Accredited/Accredited";
+// import { getAccredited } from "@/sanity/sanity.query";
+// import Page from "@/components/Accredited/Accredited";
+// import { revalidateTag } from 'next/cache'
+// import TopPageCommon from "@/components/Accredited/TopPageCommon";
+
+
+// const Accredited = async () => {
+//   revalidateTag('collection')
+//   try {
+//     const pageIndex = 0;
+//     const pageData = await getAccredited(pageIndex); // Passa l'indice corretto
+
+//     // Verifica che i dati non siano nulli
+//     if (!pageData) {
+//       console.error(
+//         "Errore nel recupero dei dati: i dati della pagina sono nulli"
+//       );
+//       return null;
+//     }
+
+//     return (<>
+//     <TopPageCommon data={commonTopPageData  }/>
+//     <Page data={pageData} pageType={pageData.pageType.current} pageIndex={pageIndex}/>
+    
+//     </>
+      
+       
+         
+      
+//     );
+//   } catch (error) {
+//     console.error("Errore nel recupero dei dati:", error);
+//     return null;
+//   }
+// };
+
+// export default Accredited;
+
+
+'use server'
+
+import { getAccredited, getCommonTopPage } from "@/sanity/sanity.query";
+import Accredited from "@/components/Accredited/Accredited";
+import TopPageCommon from '@/components/Accredited/TopPageCommon';
+
+
+
+ 
 import { revalidateTag } from 'next/cache'
 
 
-const Accredited = async () => {
+
+const AccreditedPageWrapper = async () => {
   revalidateTag('collection')
   try {
-    const pageIndex = 0;
-    const pageData = await getAccredited(pageIndex); // Passa l'indice corretto
+    const [
+      accreditedData,
+      commonTopPageData,
+  
+    ] = await Promise.all([
+      getAccredited(0),
+      getCommonTopPage(),
 
-    // Verifica che i dati non siano nulli
-    if (!pageData) {
+    ]);
+
+    // Verifica che entrambi i set di dati non siano nulli
+    if (
+      !accreditedData ||
+      ! commonTopPageData 
+
+    ) {
       console.error(
-        "Errore nel recupero dei dati: i dati della pagina sono nulli"
+        "Errore nel recupero dei dati: uno o entrambi i set di dati sono nulli"
       );
       return null;
     }
 
     return (
-      
+      <>
+        {/* Passa i dati come props ai componenti figlio */}
+        <TopPageCommon data={commonTopPageData  } />
+       <Accredited data={accreditedData} pageType={""} pageIndex={0} />
+   
+   
+        {/* <CommonTopPage data={commonTopPageData } />  */}
+ 
        
-        <Page data={pageData} pageType={pageData.pageType.current} pageIndex={pageIndex}/>
-     
-      
+
+      </>
     );
   } catch (error) {
     console.error("Errore nel recupero dei dati:", error);
@@ -32,4 +96,5 @@ const Accredited = async () => {
   }
 };
 
-export default Accredited;
+export default AccreditedPageWrapper;
+
---
+++ components/Accredited/TopPageCommon.tsx
@@ -0,0 +1,54 @@
+import styles from "./topPage.module.scss";
+import { getCommonTopPage } from "@/sanity/sanity.query";
+import { urlFor } from "@/sanity/sanity.client";
+
+
+import Image from "next/image";
+import { CommonTop as CommonTopType } from "@/Types/CommonTop";
+import Link from "next/dist/client/link";
+import { revalidateTag } from "next/cache";
+
+interface CommonTopTypeProps {
+  data: CommonTopType[]; // Accetta i dati come prop
+}
+
+export default function CommonTopPageTeachers({ data }: CommonTopTypeProps) {
+  revalidateTag("collection");
+
+  // Filtra i dati per trovare solo l'elemento desiderato
+  const specificItem = data.find(item => item._id === '6473cb9f-c4a9-4e5d-bbca-722ff643ec62');
+
+  if (!specificItem) {
+    return null; // Se l'elemento specifico non esiste, ritorna null o gestisci di conseguenza
+  }
+
+  const imageUrl = urlFor(specificItem.backgroundImage).url();
+
+
+  const { title, subtitle } = specificItem;
+
+  return (
+    <div className={styles.topMainContainer} >
+      {/* Utilizza l'URL dell'immagine nell'elemento Image */}
+      <Image
+        className={styles.imageBackground}
+        src={imageUrl}
+        // src={imageUrl}
+        alt={title}
+        sizes="100vw"
+        style={{
+          width: "100%",
+          height: "auto",
+        }}
+        width={1000}
+        height={400}
+      />
+      <div className={styles.topTextContainer}>
+      <h1 className={styles.mainTitle}>{title}</h1>
+      <p className={styles.mainText}>{subtitle}</p>
+      </div>
+      <span className={styles.square1}></span>
+        <span className={styles.square2}></span>
+
+    </div>
+  )};
