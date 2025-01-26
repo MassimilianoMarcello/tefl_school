136f76c5bb039a4f35c83219680a05319d04504d
massimiliano <massimarcello@hotmail.it>
2025-01-27 00:50:25 +0100

    modularity sass mediaquerys testing

--- components/Our teachers/OurTeachers.tsx
+++ components/Our teachers/OurTeachers.tsx
@@ -1,5 +1,5 @@
 import styles from "./teachers.module.scss";
-import { getTeachers,getCommonTopPage } from "@/sanity/sanity.query";
+import { getTeachers, getCommonTopPage } from "@/sanity/sanity.query";
 import { urlFor } from "@/sanity/sanity.client";
 
 import Image from "next/image";
@@ -9,78 +9,72 @@ import Link from "next/dist/client/link";
 import { revalidateTag } from "next/cache";
 import ButtonAzure from "../Buttons/ButtonAzure";
 
-
-
 export default async function OurTeachers() {
-revalidateTag("collection");
+  revalidateTag("collection");
   const dataTopPage = await getCommonTopPage();
-  const data= await getTeachers();
+  const data = await getTeachers();
   return (
     <>
-    <TopPageTeachers
-          data={dataTopPage}
-          dynamicStyle="ourteachersContainer"
-          id="d1b1177b-c086-4f38-86c6-ac32c794b342"
-        />
-    <main className={styles.mainContainer}>
-      <section className={styles.coursesTextContainer}>
-        <span className={styles.square3}></span>
-        <span className={styles.square4}></span>
+      <TopPageTeachers
+        data={dataTopPage}
+        dynamicStyle="ourteachersContainer"
+        id="d1b1177b-c086-4f38-86c6-ac32c794b342"
+      />
+      <main className={styles.mainContainer}>
+        <section className={styles.coursesTextContainer}>
+          <span className={styles.square3}></span>
+          <span className={styles.square4}></span>
 
-        <div className={styles.courseCardsBox}>
-          {data &&
-            data.map((teacher, index) => (
-              <div
-                key={teacher._id}
-                className={`${styles.cardContainer} ${styles[`cardContainer${index}`]}`}
-              >
-                <div className={styles.cardImageContainer}>
-                  {/* <Image
+          <div className={styles.courseCardsBox}>
+            {data &&
+              data.map((teacher, index) => (
+                <div
+                  key={teacher._id}
+                  className={`${styles.cardContainer} ${styles[`cardContainer${index}`]}`}
+                >
+                  <div className={styles.cardImageContainer}>
+                    {/* <Image
                     className={styles.cardImage}
                     src={teacher.idPhoto}
                     alt={teacher.alt}
                     width={500}
                     height={300}
                   /> */}
-                  <Image
-                    src={urlFor(teacher.idPhoto).width(300).height(300).url()}
-                    width={200}
-                    height={200}
-                    alt={teacher.alt}
-                    className={styles.cardImage}
-                  />
-                   <span className={styles.squareBoss}></span>
-                   <span className={styles.squareBoss1}></span>
-                   <span className={styles.squareBoss2}></span>
-                   <span className={styles.squareBoss3}></span>
+                    <Image
+                      src={urlFor(teacher.idPhoto).width(300).height(300).url()}
+                      width={200}
+                      height={200}
+                      alt={teacher.alt}
+                      className={styles.cardImage}
+                    />
+                    <span className={styles.squareBoss}></span>
+                    <span className={styles.squareBoss1}></span>
+                    <span className={styles.squareBoss2}></span>
+                    <span className={styles.squareBoss3}></span>
 
-
-                  <div className={styles.courseLevelContainer}>
-                    {/* <p className={styles.courseLevel}>
+                    <div className={styles.courseLevelContainer}>
+                      {/* <p className={styles.courseLevel}>
                       {teacher.infoParagraph}
                     </p> */}
+                    </div>
+                    <div className={styles.cardNameContainer}>
+                      <h3 className={styles.cardName}>{teacher.name}</h3>
+                      <p className={styles.cardRole}>{teacher.role}</p>
+                    </div>
                   </div>
-                  <div className={styles.cardNameContainer}>
-                    <h3 className={styles.cardName}>{teacher.name}</h3>
-                    <p className={styles.cardRole}>{teacher.role}</p>
-                  </div>
-                </div>
 
-                {/* <Link href={`/`} className={styles.detailsLink}>
+                  {/* <Link href={`/`} className={styles.detailsLink}>
                   Details
                 </Link> */}
-           
-              
-              </div>
-            ))}
-          
-        </div>
-        <div className={styles.buttonApplication}>
-                <ButtonAzure text={"Join The Team"}/>
-                <p>We’re always looking for talented teachers to join our team.</p>
                 </div>
-      </section>
-    </main>
+              ))}
+          </div>
+          <div className={styles.buttonApplication}>
+            <ButtonAzure text={"Join The Team"} />
+            <p>We’re always looking for talented teachers to join our team.</p>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
